9285452f092d916c19e81447c315a8fa4aa21c78
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-05-27 04:58:10 +0300

    React Failed project

--- src/App.js
+++
@@ -1,8 +0,0 @@
-// import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
-  return <div>I'm back to learning react finally, Building photohive soon</div>;
-}
-
-export default App;
